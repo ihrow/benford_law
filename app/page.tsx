9699917f6bd706e13eb9079529344fe4abc6380b
ihrow <ihrow@proton.me>
2024-04-13 14:56:19 +0200

    hammer js fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,15 @@
 import { PrimeReactProvider } from "primereact/api";
 import App from "./pages/App";
 import { Chart } from "chart.js";
-import zoomPlugin from "chartjs-plugin-zoom";
-Chart.register(zoomPlugin);
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (typeof window !== "undefined")
+      import("chartjs-plugin-zoom").then((plugin) => {
+        Chart.register(plugin.default);
+      });
+  }, []);
   return (
     <PrimeReactProvider>
       <App />
